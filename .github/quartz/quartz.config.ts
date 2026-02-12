a11268c8555b5131fb10dd5717ac0694c28043af
Avazar <JadinSadler+GitHub@protonmail.com>
2026-02-11 17:46:15 -0800

    vault backup: 2026-02-11 17:46:15

---
+++ .github/quartz/quartz.config.ts
@@ -0,0 +1,16 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "SBSWarGame",
+    enableSPA: true,
+    enablePopovers: true,
+  },
+  plugins: {
+
+  },
+}
+
+export default config
